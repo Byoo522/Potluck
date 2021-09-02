d777457aae8074a2a595e2c7f6f12e227aea7eb7
Ben <byoo0522@gmail.com>
2021-09-02 10:43:36 -0700

    fix demo login feature to login on demo click

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Home.css'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 
 
 function Home() {
+  const sessionUser = useSelector(state => state.session.user);
 
+  if (!sessionUser) return <Redirect to="/login" />;
 
   return (
     <div className='home-container'>
