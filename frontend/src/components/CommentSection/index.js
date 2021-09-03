4e1c82cea46f1ab487a0d452d293b24d580488ab
Ben <byoo0522@gmail.com>
2021-09-03 09:10:13 -0700

    style events, edit, delete buttons, and align tables properly

--- frontend/src/components/CommentSection/index.js
+++ frontend/src/components/CommentSection/index.js
@@ -4,9 +4,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import CommentForm from '../CommentForm';
 import { useParams, useHistory } from 'react-router-dom'
 import './CommentSection.css'
-// USE FOR TERNARY
-// import CommentRead from '../CommentRead';
-// import CommentEdit from '../CommentEdit';
 
 
 function CommentSection() {
@@ -14,13 +11,6 @@ function CommentSection() {
   const history = useHistory();
   const { id } = useParams();
   const comments = useSelector((state) => state?.comments)
-  // const [comment, setComment] = useState(comments)
-
-  // FOR TERNARY TOGGLE
-  // const [editCommentId, setEditCommentId] = useState(null)
-  // const [editCommentFormData, setCommentFormData] = useState({
-  //   content: ''
-  // })
 
 
   const data = {
@@ -44,71 +34,28 @@ function CommentSection() {
     history.push(`/comments/edit/${commentId}`)
   }
 
-
-
-  // FOR TERNARY TOGGLE EDIT
-  // const handleEditClick = (e, comment) => {
-  //   e.preventDefault();
-  //   setEditCommentId(comment.id);
-  //   const formValues = {
-  //     content: comment.content,
-  //   }
-
-  //   setCommentFormData(formValues)
-  // }
-
-  // const handleEditFormChange = e => {
-  //   e.preventDefault();
-  //   const fieldContent = e.target.getAttribute('content');
-  //   const fieldValue = e.target.value
-
-  //   const newFormData = { ...editCommentFormData }
-  //   newFormData[fieldContent] = fieldValue
-
-  //   setCommentFormData(newFormData)
-  // }
-
-
-
   return (
     <div className='comment-container'>
       <div className='comments'>
-        <table>
+        <table className='comment-table'>
           <thead>
             <tr>
-              <th>Comments</th>
-              <th></th>
-              <th></th>
+              <th colSpan='3'>Comments</th>
             </tr>
           </thead>
           <tbody>
             {comments && Object.values(comments).map((comment) => (
               <tr key={comment?.id}>
-                <td>
+                <td colSpan='3'>
                   {comment?.content}
                 </td>
                 <td>
-                  <button value={comment?.id} onClick={handleEditClick} className='button yellow-bg red comment-post-btn font'>Edit</button>
-                  {/* <button value={comment?.id} onClick={handleEditClick}><i class="far fa-edit"></i></button> */}
-                </td>
-                <td>
-                  <button value={comment?.id} onClick={handleDelete} className='button yellow-bg red comment-post-btn font'>Delete</button>
-                  {/* <button value={comment?.id} onClick={handleDelete}><i class="far fa-trash-alt"></i></button> */}
+                  <button value={comment?.id} onClick={handleEditClick} className='action-button'><i class="far fa-edit"></i></button>
+                  <button value={comment?.id} onClick={handleDelete} className='action-button'><i class="far fa-trash-alt"></i></button>
                 </td>
               </tr>
             ))}
           </tbody>
-          {/* refactor for ternary toggle */}
-          {/* <tbody className='comments-section'>
-            {comments && Object.values(comments).map((comment, i) => (
-              <>
-                {editCommentId === comment.id ? (
-                  <CommentEdit editCommentFormData={editCommentFormData} handleEditFormChange={handleEditFormChange} />
-                ) : (
-                  <CommentRead comment={comment} handleEditClick={handleEditClick} />)}
-              </>
-            ))}
-          </tbody> */}
         </table>
       </div>
       <CommentForm />
